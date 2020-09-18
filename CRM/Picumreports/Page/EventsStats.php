9fc00d0adf05ed66d98ca4d195903add240810d2
Alain Benbassat <alain@businessandcode.eu>
2020-09-18 20:39:33 +0200

    eventSummary

--- CRM/Picumreports/Page/EventsStats.php
+++ CRM/Picumreports/Page/EventsStats.php
@@ -17,16 +17,77 @@ class CRM_Picumreports_Page_EventsStats extends CRM_Core_Page {
     $returnURL = '<a href="' . CRM_Utils_System::url('civicrm/picummembersstats', "reset=1") . '">Return to PICUM CRM Statistics</a>';
     $this->assign('returnURL', $returnURL);
 
+    $eventSummary = $this->getEventSummary();
+    $this->assign('eventSummary', $eventSummary);
+
     $events = $this->getEvents();
     $this->assign('events', $events);
 
     parent::run();
   }
 
+  private function getEventSummary() {
+    // get a list of event types with count and number of particiapnts
+    $sql = "
+      select       
+       v.label event_type,
+       count(distinct e.id) total_events,
+       count(p.id) total_participants
+      from 
+        civicrm_event e
+      inner join
+        civicrm_option_value v on e.event_type_id = v.value   
+      inner join
+        civicrm_option_group g on v.option_group_id = g.id
+      left outer join
+        civicrm_participant p on p.event_id = e.id and p.status_id in (1,2)
+      where 
+        g.name  = 'event_type'
+      and
+        v.is_active = 1
+      AND
+        e.is_active = 1
+      and
+        year(e.start_date) = {$this->year}    
+      GROUP BY
+        v.label 
+      order by
+        v.label    
+    ";
+    $dao = CRM_Core_DAO::executeQuery($sql);
+    $r = $dao->fetchAll();
+
+    // add the totals
+    if (count($r) > 0) {
+      $sql = "
+        select    
+          count(distinct e.id) total_events,
+          count(p.id) total_participants
+        from 
+          civicrm_event e
+        left outer join
+          civicrm_participant p on p.event_id = e.id and p.status_id in (1,2)
+        where 
+          e.is_active = 1
+        and
+          year(e.start_date) = {$this->year}   
+      ";
+      $dao = CRM_Core_DAO::executeQuery($sql);
+      $dao->fetch();
+      $r[] = [
+        'event_type' => '<strong>TOTAL</strong>',
+        'total_events' => '<strong>' . $dao->total_events . '</strong>',
+        'total_participants' => '<strong>' . $dao->total_participants . '</strong>'
+      ];
+    }
+
+    return $r;
+  }
+
   private function getEvents() {
     $events = [];
 
-    // get a list of event categories
+    // get a list of event types
     $sql = "
       select 
        v.value 
@@ -39,6 +100,8 @@ class CRM_Picumreports_Page_EventsStats extends CRM_Core_Page {
         g.name  = 'event_type'
       and
         v.is_active = 1        
+      order by
+        v.label
     ";
     $dao = CRM_Core_DAO::executeQuery($sql);
     while ($dao->fetch()) {
@@ -60,6 +123,8 @@ class CRM_Picumreports_Page_EventsStats extends CRM_Core_Page {
         civicrm_participant p on p.event_id = e.id
       where 
         p.status_id in (1, 2)
+      and
+        e.is_active = 1
       and
         e.event_type_id = $eventTypeId
       and
--- templates/CRM/Picumreports/Page/EventsStats.tpl
+++ templates/CRM/Picumreports/Page/EventsStats.tpl
@@ -1,7 +1,29 @@
 <p>&lt; {$returnURL}</p>
+<h2>Summary</h2>
+<table class="report-layout display">
+    <thead>
+    <tr>
+        <th>Event Type</th>
+        <th>Total Events (per type)</th>
+        <th>Total Attended participants (per type)</th>
+    </tr>
+    </thead>
+    <tbody>
+    {foreach from=$eventSummary item=row}
+        <tr class="{cycle values="odd-row,even-row"}">
+            <td>{$row.event_type}</td>
+            <td>{$row.total_events}</td>
+            <td>{$row.total_participants}</td>
+        </tr>
+    {/foreach}
+    </tbody>
+</table>
+<p>&nbsp;</p>
 
+
+<h2>Events per Event Type</h2>
 {foreach from=$events item=rows key=eventCategory}
-    <h2>{$eventCategory}</h2>
+    <h3>{$eventCategory}</h3>
     <table class="report-layout display">
         <thead>
         <tr>
