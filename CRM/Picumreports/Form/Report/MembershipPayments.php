e5b194d49c4fa87dc26e4c6448291a0b807cfe2f
Alain Benbassat <alain@businessandcode.eu>
2024-06-13 16:08:04 +0200

    comment and receive date cols

--- CRM/Picumreports/Form/Report/MembershipPayments.php
+++ CRM/Picumreports/Form/Report/MembershipPayments.php
@@ -2,7 +2,8 @@
 use CRM_Picumreports_ExtensionUtil as E;
 
 class CRM_Picumreports_Form_Report_MembershipPayments extends CRM_Report_Form {
-  protected $_customGroupExtends = ['Membership'];
+ // protected $_customGroupFilters = TRUE;
+  //protected $_customGroupExtends = ['Membership'];
 
   function __construct() {
     $this->_columns = [
@@ -57,17 +58,31 @@ class CRM_Picumreports_Form_Report_MembershipPayments extends CRM_Report_Form {
           ],
         ],
       ],
+      'civicrm_value_membership_de_8' => [
+        'fields' => [
+          'comments_52' => [
+            'title' => 'Comment',
+            'required' => FALSE,
+            'dbAlias' => 'md.comments_52'
+          ],
+        ],
+      ],
     ];
 
     // add the years since 2012
     $currentYear = date('Y');
-    $minNumYears = 3;
     for ($y = 2012; $y <= $currentYear; $y++) {
       $this->_columns['civicrm_years']['fields']["fee_$y"] = [
         'title' => "Fee $y",
-        'default' => ($y > $currentYear - $minNumYears) ? TRUE : FALSE,
+        'default' => FALSE,
         'dbAlias' => '0.00',
         'type' => CRM_Utils_Type::T_MONEY,
+      ];
+      $this->_columns['civicrm_receive_dates']['fields']["receive_date_$y"] = [
+        'title' => "Receive date $y",
+        'default' => FALSE,
+        'dbAlias' => "'-'",
+        'type' => CRM_Utils_Type::T_STRING,
       ];
     }
 
@@ -125,6 +140,8 @@ class CRM_Picumreports_Form_Report_MembershipPayments extends CRM_Report_Form {
       inner join
         civicrm_membership m on {$this->_aliases['civicrm_contact']}.id = m.contact_id
       left outer join
+        civicrm_value_membership_de_8 md on m.id = md.entity_id
+      left outer join
         civicrm_membership_status {$this->_aliases['civicrm_membership_status']} ON {$this->_aliases['civicrm_membership_status']}.id = m.status_id
       left outer join
         civicrm_value_geographical_area_1 g on g.entity_id = {$this->_aliases['civicrm_contact']}.id
@@ -169,7 +186,8 @@ class CRM_Picumreports_Form_Report_MembershipPayments extends CRM_Report_Form {
           // get the contributions for that contact and year and staus = Completed or Pending
           $sql = "
             select
-              total_amount
+              total_amount,
+              date_format(receive_date, '%Y-%m-%d') receive_date
             from
               civicrm_contribution
             where
@@ -184,11 +202,21 @@ class CRM_Picumreports_Form_Report_MembershipPayments extends CRM_Report_Form {
             2 => ["Fee $year", 'String'],
           ];
 
-          $total = CRM_Core_DAO::singleValueQuery($sql, $sqlParams);
+          $total = null;
+          $receiveDate = null;
+          $dao = CRM_Core_DAO::executeQuery($sql, $sqlParams);
+          if ($dao->fetch()) {
+            $total = $dao->total_amount;
+            $receiveDate = $dao->receive_date;
+          }
 
           // make the background color red if the amount is zero
-          if ($total == 0) {
+          if ($total === 0) {
             $total = "<span style=\"background-color:red\">$total</span>";
+          }
+
+          if (array_key_exists("civicrm_receive_dates_receive_date_$year", $row)) {
+            $rows[$i]["civicrm_receive_dates_receive_date_$year"] = $receiveDate;
           }
 
           $rows[$i][$k] = $total;
